fd0e374b5540f17c8eee0476306378fe7f89e2a5
vawi <val.winnen@gmail.com>
2020-05-27 08:58:26 +0200

    add selenium
    test selenium for selecting element

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
-
+const firefox = require('selenium-webdriver/firefox');
 const url = 'https://chillhop.bandcamp.com/album/chillhop-essentials-spring-2020';
 
+
+const screen = {
+    width: 640,
+    height: 480
+};
+
+let driver = new Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless().windowSize(screen)).build();
+
 async function scrap() {
-    let driver = await new Builder().forBrowser('firefox').build();
     try {
         await driver.get(url);
-        await driver.findElement(driver.findElement(By.className('track_row_view linked')));
+        await driver.findElement(By.className("playbutton")).click();
+        
+        await driver.findElement(By.xpath(".//*[@id='track_table']/tbody/tr/td/div/a/span")).then(data => {
+            console.log(data.getText());
+        });
     } finally {
         await driver.quit();
     }
 }
 
-scrap();
+scrap()
