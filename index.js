3258ff360e1d1111443d2f1a627665fd054ca7f9
vawi <val.winnen@gmail.com>
2020-05-28 12:35:33 +0200

    Add findElement on span
    Get list of track ok !

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 const url = 'https://chillhop.bandcamp.com/album/chillhop-essentials-spring-2020';
 
-
 const screen = {
     width: 640,
     height: 480
 };
+
+let titleArray = [];
 
 let driver = new Builder().forBrowser('firefox').setFirefoxOptions(new firefox.Options().headless().windowSize(screen)).build();
 
 async function scrap() {
     try {
         await driver.get(url);
-        await driver.findElement(By.className("playbutton")).click();
-        
-        await driver.findElement(By.xpath(".//*[@id='track_table']/tbody/tr/td/div/a/span")).then(data => {
-            console.log(data.getText());
-        });
+        //await driver.findElement(By.className("playbutton")).click();
+
+        let elements = await driver.findElements(By.className('track-title'));
+        for(let e of elements) {
+            titleArray.push(await e.getText());
+        }
+
+        console.log(titleArray);
+
     } finally {
         await driver.quit();
     }
